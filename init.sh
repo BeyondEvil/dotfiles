95b00830db66626b21886af77c8996f5d9f50f3b
Jim Brännlund <jimbrannlund@fastmail.com>
2022-02-06 15:39:44 +0100

    Install homebrew

--- init.sh
+++ init.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-echo "Hello World"
+echo "Create folders"
+# mkdir -p $HOME/dev
+# mkdir -p $HOME/.config/zsh
+# mkdir -p $HOME/bin
 
-if ! xcode-select -p; then
+if ! xcode-select -p 1>/dev/null; then
   echo "Installing xcode command line tools..."
   xcode-select --install
+fi
+
+if ! which brew 1>/dev/null; then
+  echo "Installing Homebrew..."
+  echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 echo "Done"
