82ba0ebc07235d932e3dc56b148ecbdc2834b5be
Jim Brännlund <jimbrannlund@fastmail.com>
2022-02-10 03:12:49 +0100

    Cleanup

--- init.sh
+++ init.sh
@@ -4,75 +4,69 @@ set -eo pipefail
 
 NVM_VERSION=0.39.1
 
-echo "Create folders"
+echo "Creating folders"
 mkdir -p ~/dev
 mkdir -p ~/.config/zsh/plugins
 mkdir -p ~/bin
 
 export DEV=~/dev
 
-echo "WHAT IS DEV: ${DEV}"
-
 if ! xcode-select -p 1>/dev/null; then
-  echo "Installing xcode command line tools..."
+  echo "Installing xcode command line tools"
   xcode-select --install
 fi
-
-echo "EXPORT ZDOTDIR"
 
 # set the amazing ZDOTDIR variable
 export ZDOTDIR=~/.config/zsh
 
-echo "CLONE DOTFILES"
 # clone the dotfiles repo
 if [[ ! -d $DEV/dotfiles/.git ]]; then
-  echo "CLONING"
+  echo "Cloning dotfiles"
   git clone --recursive https://github.com/BeyondEvil/dotfiles.git $DEV/dotfiles
 else
-  echo "PULLING"
+  echo "Updating dotfiles"
   pushd $DEV/dotfiles
   git pull
   popd
 fi
 
-echo "COPY DOTFILES"
+echo "Copying dotfiles to ZDOTDIR"
 cp $DEV/dotfiles/.z* $ZDOTDIR
 
-echo "USE ZDOTDIR"
 # change the root .zshenv file to use ZDOTDIR
+if [[ -s ~/.zshenv ]]; then
 cat << 'EOF' >| ~/.zshenv
 export ZDOTDIR=~/.config/zsh
 [[ -f $ZDOTDIR/.zshenv ]] && . $ZDOTDIR/.zshenv
 EOF
-
-echo "INSTALL BREW"
+fi
 
 if ! which brew 1>/dev/null; then
-  echo "Installing Homebrew..."
+  echo "Installing Homebrew"
   echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
-echo "EVAL BREW"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-echo "INSTALL NVM"
 if [[ ! -d ~/.nvm ]]; then
-  echo "Installing NVM..."
+  echo "Installing NVM"
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v${NVM_VERSION}/install.sh | bash
 fi
 
 # zsh -c "nvm install --lts && nvm alias default $(node --version)"
 
-echo "INSTALL ZSH SYNTAX HIGHLIGHTING"
-if [[ ! -d $ZDOTDIR/plugins/zsh-syntax-highlighting/.git ]]; then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZDOTDIR/plugins/zsh-syntax-highlighting
+zsh_highlight_dir=$ZDOTDIR/plugins/zsh-syntax-highlighting
+if [[ ! -d $zsh_highlight_dir/.git ]]; then
+  echo "Installing zsh syntax highlighting"
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zsh_highlight_dir
+else
+  echo "Updating zsh syntax highlighting"
+  pushd $zsh_highlight_dir
+  git pull
+  popd
 fi
 
-echo "BREW UPDATE"
-brew update
-
-echo "BREW upgrade"
-brew upgrade
+brew update && brew upgrade
 
 formulae="
 awscli
@@ -93,8 +87,8 @@ xz
 zlib
 "
 
-echo "BREW INSTALL FORMULAE"
-brew install $formulae
+echo "Installing formulae..."
+brew install $formulae 2>/dev/null
 
 casks="
 1password
@@ -108,10 +102,10 @@ pycharm
 slack
 "
 
-echo "BREW INSTALL CASKS"
-brew install --cask $casks
+echo "Installing casks..."
+brew install --cask $casks 2>/dev/null
 
-echo "BREW CLEANUP"
+echo "Cleaning up..."
 brew cleanup
 
 echo "Done"
