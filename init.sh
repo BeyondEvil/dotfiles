a75c7aabcdfa475e1f7c3f1eefed0cf216b5bd0e
Jim Brännlund <jimbrannlund@fastmail.com>
2022-02-11 01:44:17 +0100

    install aws cli

--- init.sh
+++ init.sh
@@ -110,4 +110,15 @@ brew cleanup
 echo "Setting up python env"
 /bin/zsh -l $DEV/dotfiles/python_env.zsh
 
+if ! which aws 1>/dev/null; then
+  echo "Installing AWS CLI"
+  curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+  installer -pkg AWSCLIV2.pkg \
+    -target CurrentUserHomeDirectory \
+    -applyChoiceChangesXML $DEV/dotfiles/awscli.xml
+    ln -s ~/dev/aws-cli/aws ~/.local/bin/aws
+    ln -s ~/dev/aws-cli/aws_completer ~/.local/bin/aws_completer
+  rm "AWSCLIV2.pkg"
+fi
+
 echo "Done"
--- python_env.zsh
+++ python_env.zsh
@@ -1,49 +1,11 @@
-[[ -o login ]] && echo "Login" || echo "Non-Login"
-
 echo "Install python 3.9"
 pyenv install --skip-existing 3.9.10
 
 echo "Set python 3.9 as global"
 pyenv global 3.9.10
 
-echo "pyenv versions"
-pyenv versions
-
-echo "Which python"
-whence -p python
-python --version
-
-echo "Which python3"
-whence -p python3
-python --version
-
-echo "pip version before"
-pip --version
-
 echo "Update pip"
 python -m pip install --upgrade pip
 
-echo "pip version after"
-pip --version
-
-echo "zsh path: $path"
-echo "os PATH: $PATH"
-
-# echo "pipx ensurepath"
 echo "Install pipx"
 python -m pip install --user --upgrade pipx
-# brew install --ignore-dependencies pipx
-# pipx ensurepath
-
-
-# echo "restart the shell"
-# exec zsh -l
-
-echo "pipx version"
-pipx --version
-
-# echo "Install awscli"
-pipx install awscli
-
-# echo "AWS version"
-aws --version
