a67bc52126c6cd72aaf25f6547df869c5bca55d7
Jim Brännlund <jimbrannlund@fastmail.com>
2022-03-16 22:05:27 +0100

    fix hotcorners

--- osx.sh
+++ osx.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Set top left corner to "lock screen"
-defaults write com.apple.dock "wvous-tl-corner" = 13;
-defaults write com.apple.dock "wvous-tl-modifier" = 0;
+defaults write com.apple.dock "wvous-tl-corner" 13;
+defaults write com.apple.dock "wvous-tl-modifier" 0;
 
 # Set bottom left corner to "show desktop"
-defaults write com.apple.dock "wvous-bl-corner" = 4;
-defaults write com.apple.dock "wvous-bl-modifier" = 0;
+defaults write com.apple.dock "wvous-bl-corner" 4;
+defaults write com.apple.dock "wvous-bl-modifier" 0;
