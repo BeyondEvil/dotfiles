248313c5c0973495e331a8af3d9cbec6f88b8d83
Jim Brännlund <jimbrannlund@fastmail.com>
2022-03-16 22:06:18 +0100

    restart dock

--- osx.sh
+++ osx.sh
@@ -7,3 +7,5 @@ defaults write com.apple.dock "wvous-tl-modifier" 0;
 # Set bottom left corner to "show desktop"
 defaults write com.apple.dock "wvous-bl-corner" 4;
 defaults write com.apple.dock "wvous-bl-modifier" 0;
+
+killall Dock
